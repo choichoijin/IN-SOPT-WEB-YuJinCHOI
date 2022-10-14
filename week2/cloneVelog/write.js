8235cf7f3da35b53b21c7e559f7a93d2a667b19a
choichoijin <cuj10594@yonsei.ac.kr>
2022-10-14 14:51:35 +0900

    feat: 태그 추가시 중복 방지

--- week2/cloneVelog/write.js
+++ week2/cloneVelog/write.js
@@ -1,4 +1,5 @@
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 function attachEvent() {
   const input = $("input");
@@ -11,10 +12,17 @@ function attachEvent() {
 }
 
 function addTag(inputValue) {
-  const ul = $("ul");
+  const tagList = $("ul.tagList");
   const li = document.createElement("li");
+  const tags = $$("li.tag");
+  const tagTextArr = Array.from(tags).map((tag) => tag.innerText);
+
   li.innerText = inputValue;
-  ul.appendChild(li);
+  li.className = "tag";
+  //중복 방지.
+  tagTextArr.includes(inputValue)
+    ? alert("중복된 태그입니다.")
+    : tagList.appendChild(li);
   //태그를 클릭하면 삭제.
   li.addEventListener("click", (e) => {
     e.target.remove();
