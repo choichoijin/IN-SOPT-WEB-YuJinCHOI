bb6935a5e7a7e8590d5d7c1f9515c3bb9222440a
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-19 17:38:53 +0900

    feat: 통신 코드 작성

--- week4/src/App.tsx
+++ week4/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import axios from "axios";
 import {
   ChakraProvider,
   Flex,
@@ -8,24 +9,34 @@ import {
   Button,
 } from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Flex
-      w="100vw"
-      h="150"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      fontSize="xl"
-      borderRadius="15px"
-    >
-      <Heading m="10">Github Profile Finder</Heading>
-      <Flex>
-        <Input w={350} h={30} placeholder="Github Username"></Input>
-        <Button h={30} marginLeft={3}>
-          확인
-        </Button>
+function App() {
+  const getUserData = async () => {
+    const response = await axios.get(
+      "https://api.github.com/users/choichoijin"
+    );
+    console.log(response);
+  };
+  getUserData();
+  return (
+    <ChakraProvider theme={theme}>
+      <Flex
+        w="100vw"
+        h="150"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        fontSize="xl"
+        borderRadius="15px"
+      >
+        <Heading m="10">Github Profile Finder</Heading>
+        <Flex>
+          <Input w={350} h={30} placeholder="Github Username"></Input>
+          <Button h={30} marginLeft={3}>
+            확인
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
-  </ChakraProvider>
-);
+    </ChakraProvider>
+  );
+}
+export default App;
