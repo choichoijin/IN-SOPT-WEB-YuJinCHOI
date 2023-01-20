0a1a63abae1bb07fcba34773a1d8320e8e59a47f
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-20 14:29:41 +0900

    feat: handle axios request

--- week4/src/App.tsx
+++ week4/src/App.tsx
@@ -7,20 +7,40 @@ import {
   Input,
   Heading,
   Button,
+  Box,
+  Text,
+  Image,
 } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface DataType {
+  login: string;
+  name: string;
+  avatar_url: string;
+  followers: number;
+  following: number;
+  html_url: string;
+}
 
 function App() {
-  const [keyword, setKeyword] = React.useState("");
-  const getUserData = async () => {
-    const response = await axios.get(`https://api.github.com/users/${keyword}`);
-    console.log(response);
-  };
+  const [keyword, setKeyword] = useState("");
+  // const [isNoUser, setIsNoUser] = useState(false);
+  const [data, setData] = useState<DataType>({
+    login: "",
+    name: "",
+    avatar_url: "",
+    followers: 0,
+    following: 0,
+    html_url: "",
+  });
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   };
-  const handleSubmit = () => {
-    getUserData();
+  const handleSubmit = async () => {
+    const response = await axios.get(`https://api.github.com/users/${keyword}`);
+    setData(response.data);
   };
+  const { login, name, avatar_url, followers, following, html_url } = data;
   return (
     <ChakraProvider theme={theme}>
       <Flex
@@ -47,6 +67,36 @@ function App() {
           </Button>
         </Flex>
       </Flex>
+      <Box>
+        {login && (
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            border="gray solid 1px"
+            borderRadius="15px"
+            m={10}
+            p={5}
+          >
+            <Text fontSize={30} fontWeight={500}>
+              {name}
+            </Text>
+            <Text fontSize={20}>{login}</Text>
+            <Image src={avatar_url} w={40} h={40} m={5} borderRadius={15} />
+            <Flex gap={7}>
+              <Text fontSize={20}>
+                Followers: <b>{followers}</b>
+              </Text>
+              <Text fontSize={20}>
+                Following: <b>{following}</b>
+              </Text>
+            </Flex>
+            <Button m={5}>
+              <a href={html_url}>방문하기</a>
+            </Button>
+          </Flex>
+        )}
+      </Box>
     </ChakraProvider>
   );
 }
